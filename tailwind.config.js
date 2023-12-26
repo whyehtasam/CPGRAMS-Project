96df0c1aed75857a7f87de6e6a3c4166ab95e573
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2023-12-27 00:27:35 +0530

    intalled Flowbite a tailwind library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  "./src/**/*.{js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js",],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
