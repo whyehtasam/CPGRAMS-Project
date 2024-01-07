306042e02093097e25479dc0dbb6ff52a8bfa3f8
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-01-08 03:44:44 +0530

    Add conditional base path for production environment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/CPGRAMS-Project/",
+  base: process.env.NODE_ENV === 'production' ? '/CPGRAMS-Project/' : '/',
   plugins: [react()],
 })
