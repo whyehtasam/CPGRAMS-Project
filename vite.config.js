4c92dba68dd4cf0d785d6cb8ba47f2322b89d69c
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-01-08 03:56:09 +0530

    Add node environment to .eslintrc.cjs and update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'process';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/CPGRAMS-Project/",
-  base: process.env.NODE_ENV === 'production' ? '/CPGRAMS-Project/' : '/',
+  base: process.env.VERCEL_ENV ? '/' : '/CPGRAMS-Project/',
   plugins: [react()],
 })
